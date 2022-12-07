c03b59a7d6d22f5bc5a9413ef09edc1457a393d4
Tom Wright <tom.wright@genieai.co>
2022-12-07 21:49:21 +0000

    Add requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import distutils.core
 
 setuptools.setup(
     name="large-language-model-cli",
-    version=0.1,
+    version=0.3,
     author="Talwrii",
     author_email="Talwrii@googlemail.com",
     description="Command-line for interacting with large language models. not TUI. chatgpt",
@@ -13,5 +13,6 @@ setuptools.setup(
     packages=["llmcli"],
     long_description=open("readme.md").read(),
     entry_points={"console_scripts": ["llmcli=llmcli:main"]},
+    install_requires=["revChatGPT"],
     classifiers=[],
 )
