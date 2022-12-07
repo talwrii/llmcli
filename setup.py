0d097bdaf42bf67dd9a2377d8ad4ac567968d51c
Tom Wright <tom.wright@genieai.co>
2022-12-07 22:02:44 +0000

    Add url

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import distutils.core
 
 setuptools.setup(
     name="large-language-model-cli",
-    version=0.4,
+    version=0.5,
     author="Talwrii",
     long_description_content_type="text/markdown",
     author_email="Talwrii@googlemail.com",
     description="Command-line for interacting with large language models. not TUI. chatgpt",
     license="mit",
-    keywords="",
-    url="",
+    keywords="ChatGPT",
+    url="https://github.com/talwrii/llmcli",
     packages=["llmcli"],
     long_description=open("readme.md").read(),
     entry_points={"console_scripts": ["llmcli=llmcli:main"]},
